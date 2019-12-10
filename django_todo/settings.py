6d24ff5e59c2d5dd1320bda622651337efa752b1
Luigi Langella <luigi76langella@gmail.com>
2019-12-10 18:05:21 +0000

    Connect to heroku postgreesql database and migrate files to it

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,13 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = { 'default': dj_database_url.parse("postgres://tpwsmzrezvzvsd:55961f1c84a00058c1196f927840436ab9be0534da6ac47a70c4cf6e70afd495@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/dfq2q5qme6ps1d")}
 
 
 # Password validation
